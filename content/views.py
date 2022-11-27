fe2eef2c5627bb552d6cc8f1dbbaab85631b8d23
swBang <tmddnjs513@gmail.com>
2022-11-27 16:45:14 +0900

    add: db create, feed_list

--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') # 피드에 있는 모든 데이터를 가져옴(쿼리셋) = select * from content_feed, 최신 글을 위한 역순 출력
+        
+        return render(request, 'jinstagram/main.html', context = dict(feed_list=feed_list))
 
-# Create your views here.
+
