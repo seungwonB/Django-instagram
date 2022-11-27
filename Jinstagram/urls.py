bec7b0cbe62cbe042a3aa99ca3a5b0ea343dfda8
swBang <tmddnjs513@gmail.com>
2022-11-28 00:02:29 +0900

    feat: login, join UI View

--- Jinstagram/urls.py
+++ Jinstagram/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import Sub
 from content.views import Main
 from content.views import UploadFeed
@@ -24,7 +24,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', Main.as_view()),
-    path('content/upload', UploadFeed.as_view())
+    # 다른 앱에 있는 url
+    path('content/',include('content.urls')),
+    path('user/', include('user.urls'))
 ]
 
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
