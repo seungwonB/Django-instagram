578dd165a6348ad32b95d14fc852c98985a4c177
swBang <tmddnjs513@gmail.com>
2022-11-28 01:23:11 +0900

    feat: Login function

--- user/views.py
+++ user/views.py
@@ -28,4 +28,16 @@ class Login(APIView):
 
     def post(self, request):
         # 로그인
-        pass
+        email = request.data.get("email", None)
+        password = request.data.get('password', None)
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return Response(status=404, data=dict(message="회원정보가 잘못되었습니다.")) # 해킹 방지
+
+        if user.check_password(password):
+            # 로그인을 했다. 세션 or 쿠키에 넣는다.
+            return Response(status=200)
+        else:
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다.")) # 해킹 방지
