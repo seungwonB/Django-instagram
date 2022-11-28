5e364338487841ab3195205b31314db32ae6f643
swBang <tmddnjs513@gmail.com>
2022-11-28 18:23:20 +0900

    fix: views typo

--- content/views.py
+++ content/views.py
@@ -15,16 +15,16 @@ class Main(APIView):
 
         for feed in feed_object_list:
             user = User.objects.filter(email=feed.email).first()
-            reply_obejct_list = Reply.objects.filter(feed_id=feed.id)
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
             reply_list = []
 
-            for reply in reply_obejct_list:
+            for reply in reply_object_list:
                 user = User.objects.filter(email=reply.email).first()
-
                 reply_list.append(dict(reply_content=reply.reply_content,
                                        nickname=user.nickname))
 
-            feed_list.append(dict(image=feed.image,
+            feed_list.append(dict(id=feed.id,
+                                  image=feed.image,
                                   content=feed.content,
                                   like_count=feed.like_count,
                                   profile_image=user.profile_image,
@@ -79,8 +79,8 @@ class Profile(APIView):
 
 class UploadReply(APIView):
     def post(self, request):
-        feed_id = request.session.get("feed_id", None)
-        reply_content = request.session.get("reply_content", None)
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
         email = request.session.get('email', None)
 
         Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
