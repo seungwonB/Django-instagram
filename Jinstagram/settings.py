678c2ceef772e7ae04660058384ffba631532323
swBang <tmddnjs513@gmail.com>
2022-11-30 09:52:41 +0900

    fix: secret code

--- Jinstagram/settings.py
+++ Jinstagram/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, json
+
+from django.core.exceptions import ImproperlyConfigured
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%06!r^28tw-g4d4fq!eo6=xudn^#_zsb8w%sfzw(-!#3f&yiuk'
+secret_file = os.path.join(BASE_DIR, 'secret.json') #secrets.json을 불러와 줍니다.
+with open(secret_file, 'r') as f:
+    secrets = json.loads(f.read())
 
+def get_secret(setting, secrets=secrets): #예외 처리를 통해 오류 발생을 검출합니다.
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
